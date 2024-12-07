63310644d82699b216e5123d7cad7f4a29134f2c
paullyy8 <>
2024-12-07 16:48:59 +0530

    add : javascript

---
+++ script/main.js
@@ -0,0 +1,10 @@
+// Start Game button functionality
+document.getElementById("start-game").addEventListener("click", () => {
+  alert("Under Construction");
+  // Replace alert with game logic
+});
+
+// Exit Game button functionality
+document.getElementById("exit-game").addEventListener("click", () => {
+  window.close(); // Closes the current browser tab (might not work on all browsers)
+});
