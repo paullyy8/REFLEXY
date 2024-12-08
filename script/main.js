b3f88e2e974e909a9beace0e1449c8167f51e21a
paullyy8 <>
2024-12-08 16:04:03 +0530

    fixed : typo

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 // Start Game button functionality
 document.getElementById("start-game").addEventListener("click", () => {
-  location.href = "/game.html"; // Redirect to the game page
+  window.location.href = '/game.html'; // Redirect to the game page
 });
 
 // Exit Game button functionality
 document.getElementById("exit-game").addEventListener("click", () => {
-  window.close(); // Closes the current browser tab (might not work on all browsers)
+  window.close(); // Closes the current browser tab (may not work on all browsers)
 });
